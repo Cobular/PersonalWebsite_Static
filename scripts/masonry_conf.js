8afa2c1d9ad6439d4196fa1c5b0d943e16eb15a4
Cobular <22972550+Cobular@users.noreply.github.com>
2020-06-16 17:49:25 -0700

    Now uses adaptive sizes, it's somewhat better, somewhat worse.
    Might switch back to static sizes w/ media queries and just add media query based column numbers

--- scripts/masonry_conf.js
+++ scripts/masonry_conf.js
@@ -1,8 +1,7 @@
 var elem = document.querySelector('.grid');
 var msnry = new Masonry( elem, {
     // options
-    itemSelector: '.grid-item',
-    columnWidth: 240,
-    gutter: 10,
-    fitWidth: true
+    itemSelector: '.grid-item-padding',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
 });
