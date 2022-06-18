effae5dd6c76adac75db71c673e7134df623f6f2
Cobular <22972550+Cobular@users.noreply.github.com>
2022-06-17 22:44:25 -0700

    Made it more gay

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import "./App.scss";
 import { Main } from "./components/Main";
 
 function App() {
+  if (window.location.host.endsWith("cobular.gay")) {
+    document.body.classList.add("gay")
+  }
+
   return (
     <div className="App">
       <Main />
